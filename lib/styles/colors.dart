8d0fe7d4cae99c4d405de6a30782c894f1c4dc1e
OLayemii <donyemisco@gmail.com>
2020-03-10 10:23:40 +0100

    Start styling

---
+++ lib/styles/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = const Color(0xffE65821);
+const lightGrey = Color(0xffE7E8EB);
+const basicGrey = Color(0xffA2A7A6);
+const brownColor = Color(0xff302B26);
+const darkGrey = Color(0xff64625A);
+const greyVariantTwo = Color(0xff7A8285);
+const lightOrange = Color(0xffD2BDAF);
+const orangeVariantTwo = Color(0xffC08F6E);
---
+++ lib/styles/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:flute/styles/colors.dart';
+
+/*
+ * Still a lot to check when there is a working emulator
+ */
+
+final lightTheme = ThemeData(
+  primaryColor: primaryColor,
+  primaryColorLight: lightOrange,
+  accentColor: primaryColor,
+  disabledColor: lightOrange,
+  backgroundColor: lightOrange,
+  fontFamily: "Poppins",
+);
